866089240722a36469bb8e65c1bb335f7645db13
Mario Rene Torres <volaras23@gmail.com>
2015-05-03 20:09:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 package com.craftilandia.friendlymobs;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin implements Listener {
 	@Override
@@ -16,59 +23,53 @@ public class Main extends JavaPlugin implements Listener {
 		if (e.getEntity() instanceof Player){
 		Player p = (Player)e.getEntity();
 		if (p.getInventory().getHelmet() != null) {
+			//very important null check so it not gives null when nothing is on helmet
+			//the line up wheres getHelmet() != null
 			if(p.getInventory().getHelmet().getType().equals(Material.SKULL_ITEM)){
 				e.setCancelled(true);//Monsters set to make no damage
-			}}
-			if(event.getEntity() instanceof Zombie) {
-			  if(event.getEntity().getType().equals(EntityType.ZOMBIE)){
-			  ItemStack head = new ItemStack(Material.SKULL_ITEM, 1);
-			  SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
-			  head.setDurability((short) 2);
-			  head.setItemMeta(skullMeta);
-		  double d = Math.random();
-		  if (d < 0.01){ 
-			  event.getDrops().add(head);
-		  }}
-		  }
-		  if(event.getEntity() instanceof Skeleton) {
-			  if(event.getEntity().getType().equals(EntityType.SKELETON)){
-			  ItemStack head = new ItemStack(Material.SKULL_ITEM, 1);
-			  SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
-			  head.setDurability((short) 0); // 0 is Skeleton
-			  head.setItemMeta(skullMeta);
-			  //SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
-			  //head.setDurability((short) 3);
-			  //skullMeta.setOwner(event.getEntity().getDisplayName());
-			  //head.setItemMeta(skullMeta);
-		  double d = Math.random();
-		  if (d < 0.01){ 
-			  event.getDrops().add(head);
-		  }}
-		  }
-		  if(event.getEntity() instanceof Player) {
-			  Player p = (Player)event.getEntity();
-			  ItemStack head = new ItemStack(Material.SKULL_ITEM, 1);
-			  SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
-			  head.setDurability((short) 3); // 3 is for player head
-			  skullMeta.setOwner(p.getPlayer().getDisplayName());
-			  head.setItemMeta(skullMeta);
-		  double d = Math.random();
-		  if (d < 0.05){ //5% of prob
-			  event.getDrops().add(head);
-		  }
-		  }
-		  if(event.getEntity() instanceof Creeper) {
-			  ItemStack head = new ItemStack(Material.SKULL_ITEM, 1);
-			  SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
-			  head.setDurability((short) 4); // 4 is for Creeper head
-			  head.setItemMeta(skullMeta);
-		  double d = Math.random();
-		  if (d < 0.01){// 1% of prob 
-			  event.getDrops().add(head);
-		  }
-		  }
-			
-		}}
-	
-	
-}
+			}}}}
+	  @EventHandler
+	  public void onEntityDeath(EntityDeathEvent event){
+		  if(event.getEntity() instanceof Zombie) {
+		  			  if(event.getEntity().getType().equals(EntityType.ZOMBIE)){
+		  			  ItemStack head = new ItemStack(Material.SKULL_ITEM, 1);
+		  			  SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+		  			  head.setDurability((short) 2);
+		  			  head.setItemMeta(skullMeta);
+		  		  double d = Math.random();
+		  		  if (d < 0.01){ 
+		  			  event.getDrops().add(head);
+		  		  }}
+		  		  }
+		  		  if(event.getEntity() instanceof Skeleton) {
+		  			  if(event.getEntity().getType().equals(EntityType.SKELETON)){
+		  			  ItemStack head = new ItemStack(Material.SKULL_ITEM, 1);
+		  			  SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+		  			  head.setDurability((short) 0); 
+		  			  head.setItemMeta(skullMeta);
+		  		  double d = Math.random();
+		  		  if (d < 0.01){ 
+		  			  event.getDrops().add(head);
+		  		  }}
+		  		  }
+		  		  if(event.getEntity() instanceof Player) {
+		  			  Player p = (Player)event.getEntity();
+		  			  ItemStack head = new ItemStack(Material.SKULL_ITEM, 1);
+		  			  SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+		  			  head.setDurability((short) 3); // 3 is for player head
+		  			  skullMeta.setOwner(p.getPlayer().getDisplayName());
+		  			  head.setItemMeta(skullMeta);
+		  		  double d = Math.random();
+		  		  if (d < 0.05){ //5% of prob
+		  			  event.getDrops().add(head);
+		  		  }
+		  		  }
+		  		  if(event.getEntity() instanceof Creeper) {
+		  			  ItemStack head = new ItemStack(Material.SKULL_ITEM, 1);
+		  			  SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+		  			  head.setDurability((short) 4); // 4 is for Creeper head
+		  			  head.setItemMeta(skullMeta);
+		  		  double d = Math.random();
+		  		  if (d < 0.01){// 1% of prob 
+		  			  event.getDrops().add(head);
+		  		  }}}}
